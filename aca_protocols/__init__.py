7e6f121cacacc38d98c135db9efbfa92a2d4b773
tobinio <Tobias.frischmann1@gmail.com>
2024-10-11 10:11:51 +0200

    lint

--- aca_protocols/__init__.py
+++ aca_protocols/__init__.py
@@ -1,5 +1,11 @@
 from .station_query_protocol import StationQueryRequest, StationQueryResponse
 from .station_register_protocol import StationRegisterRequest, StationRegisterResponse
 
-__all__ = ["StationQueryRequest", "StationQueryResponse", "StationRegisterRequest", "StationRegisterResponse",
-           "station_query_protocol", "station_register_protocol"]
+__all__ = [
+    "StationQueryRequest",
+    "StationQueryResponse",
+    "StationRegisterRequest",
+    "StationRegisterResponse",
+    "station_query_protocol",
+    "station_register_protocol",
+]
