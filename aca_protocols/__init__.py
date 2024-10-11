b9ff833edfcceb957230a37b4734a5a819e23f2c
Sebastian Plasek <s.plasek41@gmail.com>
2024-10-11 10:46:32 +0200

    added acs_id to __init__

--- aca_protocols/__init__.py
+++ aca_protocols/__init__.py
@@ -1,5 +1,6 @@
 from .station_query_protocol import StationQueryRequest, StationQueryResponse
 from .station_register_protocol import StationRegisterRequest, StationRegisterResponse
+from .acs_registry_id import acs_id
 
 __all__ = [
     "StationQueryRequest",
@@ -8,4 +9,5 @@ __all__ = [
     "StationRegisterResponse",
     "station_query_protocol",
     "station_register_protocol",
+    "acs_id",
 ]
