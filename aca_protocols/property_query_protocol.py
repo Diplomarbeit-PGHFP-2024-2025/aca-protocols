b3c54e3d6de68c517bf2daebae4f9ec2a9afe5dd
Jonas <jonaspfeifer@drei.at>
2024-10-23 07:03:48 +0000

    added comments + reformatted

--- aca_protocols/property_query_protocol.py
+++ aca_protocols/property_query_protocol.py
@@ -1,13 +1,18 @@
 from uagents import Model
 
+
 class PropertyQueryRequest(Model):
     pass
 
+
 class PropertyQueryResponse(Model):
+    # The first value in the tuple represents the seconds since 1970 when the timeframe starts
+    # The second value in the tuple represents the seconds since 1970 when the timeframe ends
     open_time_frames: list[(int, int)]
+    # Representing long and lat of the station
     geo_point: (float, float)
     cost_per_kwh: float
     charging_wattage: int
     green_energy: bool
-    station_provider: str
-    energy_provider: str
+    station_provider_name: str
+    energy_provider_name: str
