c0dda6f7955669093efcfffd2bb884c26afb8401
Jonas <jonaspfeifer@drei.at>
2024-10-23 07:12:42 +0000

    changed comments and removed unnecessary fields

--- aca_protocols/property_query_protocol.py
+++ aca_protocols/property_query_protocol.py
@@ -6,13 +6,11 @@ class PropertyQueryRequest(Model):
 
 
 class PropertyQueryResponse(Model):
-    # The first value in the tuple represents the seconds since 1970 when the timeframe starts
-    # The second value in the tuple represents the seconds since 1970 when the timeframe ends
+    # First value: unix-timestamp when the timeframe starts
+    # Second value: unix-timestamp when the timeframe ends
     open_time_frames: list[(int, int)]
     # Representing long and lat of the station
     geo_point: (float, float)
     cost_per_kwh: float
     charging_wattage: int
     green_energy: bool
-    station_provider_name: str
-    energy_provider_name: str
