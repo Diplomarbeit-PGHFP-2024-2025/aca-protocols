9ebc4aec3d3fafdaa1dbf122f3c1f30e3677c0a6
Jonas <jonaspfeifer@drei.at>
2024-11-13 11:56:57 +0000

    fixed order of objects

--- aca_protocols/property_query_protocol.py
+++ aca_protocols/property_query_protocol.py
@@ -1,16 +1,9 @@
 from typing import Tuple
-
+import json
 from uagents import Model
 
 
-class PropertyQueryRequest(Model):
-    pass
-
-
-class PropertyQueryResponse(Model):
-    properties: PropertyData
-
-class PropertyData():
+class PropertyData:
     # First value: unix-timestamp when the timeframe starts
     # Second value: unix-timestamp when the timeframe ends
     open_time_frames: list[Tuple[int, int]]
@@ -21,12 +14,16 @@ class PropertyData():
     green_energy: bool
 
     def toJson(self):
-        return json.dumps(
-            self,
-            default=lambda o: o.__dict__,
-            sort_keys=True,
-            indent=4)
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     @staticmethod
     def fromJson(data: str):
         return json.loads(data, object_hook=lambda d: PropertyData(**d))
+
+
+class PropertyQueryRequest(Model):
+    pass
+
+
+class PropertyQueryResponse(Model):
+    properties: PropertyData
