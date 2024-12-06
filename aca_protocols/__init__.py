a3ce34417c98e06aafa0b47dba8b29bb7f713dc1
leToxic <sebastian.plasek@htlstp.at>
2024-12-06 09:20:17 +0000

    payment added

--- aca_protocols/__init__.py
+++ aca_protocols/__init__.py
@@ -7,6 +7,7 @@ from .property_query_protocol import (
 )
 from .station_register_protocol import StationRegisterRequest, StationRegisterResponse
 from .acs_registry_id import acs_id
+from .ac_payment_protocol import PaymentRequest, TransactionInfo, MIN_TEST_AMOUNT, DENOM
 
 __all__ = [
     "StationQueryRequest",
@@ -23,4 +24,8 @@ __all__ = [
     "property_query_protocol",
     "car_register_protocol",
     "acs_id",
+    "PaymentRequest",
+    "TransactionInfo",
+    "MIN_TEST_AMOUNT",
+    "DENOM",
 ]
---
+++ aca_protocols/ac_payment_protocol.py
@@ -0,0 +1,18 @@
+from uagents import Model
+
+
+class PaymentRequest(Model):
+    wallet_address: str
+    amount: int
+    denomination: str
+
+    # In blockchain, "denomination" refers to the unit of value
+    # or currency in which transactions and balances are expressed.
+
+
+class TransactionInfo(Model):
+    tx_hash: str
+
+
+MIN_TEST_AMOUNT = 100
+DENOM = "atestfet"  # Smallest unit of the Fetch.ai cryptocurrency "FET"
