23c703e421103399b22c956f11fae7177693cbc3
Benedikt Huff <benedikt.huff@htlstp.at>
2024-12-13 09:38:34 +0000

    added car PropertyData

--- aca_protocols/property_query_protocol.py
+++ aca_protocols/property_query_protocol.py
@@ -21,6 +21,22 @@ class PropertyData(Model):
         return json.loads(data, object_hook=lambda d: PropertyData(**d))
 
 
+class PropertyCarData(Model):
+    green_energy: bool
+    cost_per_kwh: float
+    charging_wattage: int
+
+    max_km: int
+    time_frames: list[(int, int)]
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    @staticmethod
+    def from_json(data: str):
+        return json.loads(data, object_hook=lambda d: PropertyData(**d))
+
+
 class PropertyQueryRequest(Model):
     pass
 
